55304e8057aadad40807c7c97db3db4e85c6a214
Michał Tymejczyk <michal.tymejczyk@gmail.com>
2020-06-10 21:05:58 +0200

    Fix #1 Join

--- Join/Program.cs
+++ Join/Program.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 
-
 using static System.Console;
 using static System.Text.RegularExpressions.Regex;
 
 namespace Join
 {
+    public static class EnumerableExtension
+    {
+        public static T GetRandom<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.OrderBy(x => Guid.NewGuid()).First();
+        }
+    }
+
     public static class StringExtension 
     {
         public static string SeparateByUppercase(this string str)
@@ -20,35 +27,33 @@ namespace Join
     {
         static void Main(string[] args)
         {
+            var itemIds = new int[] { 3, 2, 3, 4, 3, 4, 1 };
             var itemNames = new string[] { "Biscuit", "Chocolate", "Butter", "Bread", "Honey" };
+            var quantities = new int[] { 800, 650, 900, 700, 900, 650, 458 };
 
             var ItemId = 1;
             var items = itemNames.Select(ItemName => (ItemId: ItemId++, ItemName)).ToList();
 
-            var quantities = new int[] { 800, 650, 900, 700, 900, 650, 458 };
-            var itemIds = new int[] { 3, 2, 3, 4, 3, 4, 1 };
-
             var InvoiceNo = 100;
-            var products = Enumerable.Zip(quantities, itemIds, (quantity, itemId) => (PurchaseQuantity: quantity, ItemId: itemId))
-                                     .Select(zipped => (InvoiceNo: InvoiceNo++, zipped.ItemId, zipped.PurchaseQuantity))
-                                     .OrderBy(p => p.ItemId)
-                                     .ToList();
+            var purchases = Enumerable.Zip(quantities, itemIds, (quantity, itemId) => (PurchaseQuantity: quantity, ItemId: itemId))
+                                      .Select(p => (InvoiceNo: InvoiceNo++, p.ItemId, p.PurchaseQuantity))
+                                      .OrderBy(p => p.ItemId)
+                                      .ToList();
 
-            (var firstItem, var firstProduct) = (items[0], products[0]);
+            (var randomItem, var randomPurchase) = (items.GetRandom(), purchases.GetRandom());
 
             var namesWithPadding = new List<(string Name, int Padding)>
             {
-                (nameof(firstProduct.ItemId),            0),
-                (nameof(firstItem.ItemName),            14),
-                (nameof(firstProduct.PurchaseQuantity), 20)
-
+                (nameof(randomPurchase.ItemId),            0),
+                (nameof(randomItem.ItemName),             14),
+                (nameof(randomPurchase.PurchaseQuantity), 20)
             }.Select(item => (Name: item.Name.SeparateByUppercase(), item.Padding)).ToList();
 
             namesWithPadding.ForEach(item => Write($"{item.Name.PadLeft(item.Padding)}"));
             
             WriteLine($"\n=================================================");
-            
-            products.ForEach(product => WriteLine($"{product.ItemId} {items.Find(item => item.ItemId == product.ItemId).ItemName, 19} {product.PurchaseQuantity, 19}"));
+
+            purchases.ForEach(product => WriteLine($"{product.ItemId,0} {items.Find(item => item.ItemId == product.ItemId).ItemName,19} {product.PurchaseQuantity,19}"));
         }
     }
 }
